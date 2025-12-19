095ae7df913343ff030d8bd0e92f9b9f1b9ad189
charu-pat <charu.patel@bbc.co.uk>
2025-12-19 12:57:36 +0000

    filters out current ingredients when getting new one

--- src/components/addFoodToGroupButton.tsx
+++ src/components/addFoodToGroupButton.tsx
@@ -13,7 +13,7 @@ export default function AddFoodButton(props: foodButtonProps) {
 
     const [idCounter,setIdCounter] = useState(0);
     const [initFoodEls, setInitFoodEls] = useState<string[]>([]);
-    const [foodEl,setFoodEl] = useState<{key: number , value:React.JSX.Element}[] | null>(null);
+    const [foodEl,setFoodEl] = useState<{key: number, name: string, value:React.JSX.Element}[] | null>(null);
     const searchParams = useSearchParams();
     const userAllergies: { id: number; value: string }[] = searchParams.getAll("allergiesCheckbox").map((allergy: string,
          index: number): { id: number; value: string } => {return {id: index, value: allergy}})
@@ -31,12 +31,35 @@ export default function AddFoodButton(props: foodButtonProps) {
         }
     })();
 
+    function newIngredientObject(ingredient: string, key: number) {
+        const foodToAdd: {key: number, name: string, value:React.JSX.Element} = {
+            key: key,
+            name: ingredient,
+            value: 
+                <div className="food-option" key={key}>
+                <div className="food-svgs">
+                    <button className='dropdown-btn svg-btn' onClick={() => dropdownOptions()} aria-label="Show Other Options">
+                        <FoodSvg className='dropdown' groupName={props.groupName} ingredient={ingredient}/>
+                    </button>
+                    <button className='refresh-btn svg-btn' onClick={() => updateIngredient(key)} aria-label="Refresh">
+                        <FoodSvg className='refresh' groupName={props.groupName} ingredient={ingredient}/>
+                    </button>
+                    <button className='cross-btn svg-btn' onClick={() => removeThisFood(key)} aria-label="Remove">
+                        <FoodSvg className='cross' ingredient={ingredient}/>
+                    </button>
+                </div>
+                <p className="food-name">{ingredient}</p>
+                </div>
+        };
+        return foodToAdd
+    }
+
     useEffect(() => {
         setInitFoodEls([]);
         const fetchfoodElems = async () => {
             let initFoodEls: string[] = [];
             for (let i = 0; i < props.groupSize; i++ ) {
-                const ingredient = await getIngredient(props.groupName);
+                const ingredient = await getIngredient(props.groupName,initFoodEls);
                 initFoodEls.push(ingredient);
             setInitFoodEls([...initFoodEls]);
             }
@@ -50,35 +73,25 @@ export default function AddFoodButton(props: foodButtonProps) {
         let tempFoodEls = [];
         for (let i = 0; i < props.groupSize; i++) {
             const newIngredient = initFoodEls[i];
-            const newFoodEl: {key: number , value:React.JSX.Element} = {
-                key: i,
-                value: 
-                    <div className="food-option" key={i}>
-                    <div className="food-svgs">
-                        <button className='dropdown-btn svg-btn' onClick={() => dropdownOptions()} aria-label="Show Other Options">
-                            <FoodSvg className='dropdown' groupName={props.groupName} ingredient={newIngredient}/>
-                        </button>                        <button className='refresh-btn svg-btn' onClick={() => updateIngredient(i)} aria-label="Refresh">
-                            <FoodSvg className='refresh' groupName={props.groupName} ingredient={newIngredient}/>
-                        </button>                        
-                        <button className='cross-btn svg-btn' onClick={() => removeThisFood(i)} aria-label="Remove ">
-                            <FoodSvg className='cross' ingredient={newIngredient}/>
-                        </button>
-                    </div>
-                    <p className="food-name">{newIngredient}</p>
-                    </div>
-            };
+            const newFoodEl: {key: number, name: string, value:React.JSX.Element} = newIngredientObject(newIngredient,i)
             tempFoodEls.push(newFoodEl);
         }
         setFoodEl(tempFoodEls);
         setIdCounter(props.groupSize);    
     },[initFoodEls]);
 
-    async function getIngredient(foodGroup: string): Promise<string> {
-        try {
+    function getCurrentIngredientsAsStrings() {
+        return foodEl?.map((el) => el.name)
+    }
+
+    async function getIngredient(foodGroup: string, currentIngredients?: string[]): Promise<string> {
+        try { 
+            const currentIngrs = currentIngredients ?? getCurrentIngredientsAsStrings()
             let userData = {
                 category: foodGroup,
                 allergies: userAllergies,
-                restrs: restrMap
+                restrs: restrMap,
+                currentIngredients: currentIngrs
             }
             const response = await axios.post('http://localhost:5432/getIngredient', {data: userData})
             if(response.data) { return response.data.name } else { return "No Options" }
@@ -90,10 +103,12 @@ export default function AddFoodButton(props: foodButtonProps) {
 
     async function getIngredients(foodGroup: string): Promise<Object[]|null> {
         try {
+            const currentIngrs = getCurrentIngredientsAsStrings()
             let userData = {
                 category: foodGroup,
                 allergies: userAllergies,
-                restrs: restrMap
+                restrs: restrMap,
+                currentIngredients: currentIngrs
             }
             const response = await axios.post('http://localhost:5432/getIngredients', {data: userData})
             let ingredientOptions: Object[] = [];
@@ -111,27 +126,10 @@ export default function AddFoodButton(props: foodButtonProps) {
         const ingredients = await getIngredients(props.groupName);
         alert(ingredients);
     }
-
+    
     const addNewFood =  async () => {
         const ingredient = await getIngredient(props.groupName);
-        const foodToAdd: {key: number , value:React.JSX.Element} = {
-            key: idCounter,
-            value: 
-                <div className="food-option" key={idCounter}>
-                <div className="food-svgs">
-                    <button className='dropdown-btn svg-btn' onClick={() => dropdownOptions()} aria-label="Show Other Options">
-                        <FoodSvg className='dropdown' groupName={props.groupName} ingredient={ingredient}/>
-                    </button>
-                    <button className='refresh-btn svg-btn' onClick={() => updateIngredient(idCounter)} aria-label="Refresh">
-                        <FoodSvg className='refresh' groupName={props.groupName} ingredient={ingredient}/>
-                    </button>
-                    <button className='cross-btn svg-btn' onClick={() => removeThisFood(idCounter)} aria-label="Remove">
-                        <FoodSvg className='cross' ingredient={ingredient}/>
-                    </button>
-                </div>
-                <p className="food-name">{ingredient}</p>
-                </div>
-        };
+        const foodToAdd: {key: number, name: string, value:React.JSX.Element} = newIngredientObject(ingredient, idCounter);
         if (foodEl==null) {setFoodEl([foodToAdd]);}
         else {setFoodEl([...foodEl, foodToAdd]);}
         setIdCounter(idCounter+1);
@@ -147,24 +145,7 @@ export default function AddFoodButton(props: foodButtonProps) {
         const currentFoodEl = foodEl?.filter(food => food.key === currentKey);
         if (currentFoodEl) {
             const newIngredient = await getIngredient(props.groupName);
-            const newFoodEl: {key: number , value:React.JSX.Element} = {
-                key: currentKey,
-                value: 
-                    <div className="food-option" key={currentKey}>
-                    <div className="food-svgs">
-                        <button className='dropdown-btn svg-btn' onClick={() => dropdownOptions()} aria-label="Show Other Options">
-                            <FoodSvg className='dropdown' groupName={props.groupName} ingredient={newIngredient}/>
-                        </button>                        
-                        <button className='refresh-btn svg-btn' onClick={() => updateIngredient(currentKey)} aria-label="Refresh">
-                            <FoodSvg className='refresh' groupName={props.groupName} ingredient={newIngredient}/>
-                        </button>                        
-                        <button className='cross-btn svg-btn' onClick={() => removeThisFood(currentKey)} aria-label="Remove ">
-                            <FoodSvg className='cross' ingredient={newIngredient}/>
-                        </button>
-                    </div>
-                    <p className="food-name">{newIngredient}</p>
-                    </div>
-            };
+            const newFoodEl: {key: number, name: string, value:React.JSX.Element} = newIngredientObject(newIngredient,currentKey); 
             setFoodEl(currentFoodEl => (currentFoodEl == null) ? null : currentFoodEl.map(food => food.key===currentKey ? newFoodEl : food))
         } 
     }
